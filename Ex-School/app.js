c17aff9750c2ec88aeaa11c5fea4446aa1a66392
hoanghuynh06599 <hoanghuynh.skyf@gmail.com>
2024-01-28 14:39:46 +0700

    Add homework generate random text when click into square

--- Ex-School/app.js
+++ Ex-School/app.js
@@ -1,7 +1,17 @@
 // Bài về nhà: thực hiện click vào 1 ô vuông xuất hiện một nội dung bất kỳ.
 
-const mobile = ['HTC', 'Nokia',  'SamSung', 'LG', 'Apple', 'Lenovo']
+const listRender = document.getElementById('list-render')
+const fakeParagraph = document.getElementById('fake-paragraph').value
 
-mobile.forEach((item) => {
-    document.write(item + '<br />');
-})
+const handleGenerationText = () => {
+    const startIndex = Math.floor(Math.random() * fakeParagraph.length)
+    const endIndex = Math.floor(Math.random() * fakeParagraph.length)
+    alert(fakeParagraph.substring(startIndex, endIndex))
+}
+
+for (let i = 0; i < 50; i++) {
+    const liTag = document.createElement('li')
+    liTag.classList.add('w-10', 'h-10', 'bg-yellow-400', 'col-1', 'cursor-pointer')
+    liTag.onclick = handleGenerationText
+    listRender.appendChild(liTag)
+}
