1def72d49b9b8dfcbf1845e6bcd5f19859bb1fc2
hoanghuynh06599 <hoanghuynh.skyf@gmail.com>
2024-02-26 23:01:34 +0700

    Add homework

--- Ex-School/app.js
+++ Ex-School/app.js
@@ -1,17 +1,35 @@
-// Bài về nhà: thực hiện click vào 1 ô vuông xuất hiện một nội dung bất kỳ.
+const menProducts = [
+    {
+        id: 1,
+        img: './assets/imgs/product-1.jpg',
+        name: 'Sản phẩm 1',
+        oldPrice: '370.000 VNĐ',
+        newPrice: '269.000 VNĐ'
+    },
+    {
+        id: 2,
+        img: './assets/imgs/product-2.jpg',
+        name: 'Sản phẩm 2',
+        oldPrice: '369.000 VNĐ',
+        newPrice: '249.000 VNĐ'
+    },
+    {
+        id: 3,
+        img: './assets/imgs/product-3.jpg',
+        name: 'Sản phẩm 3',
+        oldPrice: '390.000 VNĐ',
+        newPrice: '159.000 VNĐ'
+    },
+    {
+        id: 4,
+        img: './assets/imgs/product-4.jpg',
+        name: 'Sản phẩm 4',
+        oldPrice: '480.000 VNĐ',
+        newPrice: '209.000 VNĐ'
+    },
+]
 
-const listRender = document.getElementById('list-render')
-const fakeParagraph = document.getElementById('fake-paragraph').value
-
-const handleGenerationText = () => {
-    const startIndex = Math.floor(Math.random() * fakeParagraph.length)
-    const endIndex = Math.floor(Math.random() * fakeParagraph.length)
-    alert(fakeParagraph.substring(startIndex, endIndex))
-}
-
-for (let i = 0; i < 50; i++) {
-    const liTag = document.createElement('li')
-    liTag.classList.add('w-10', 'h-10', 'bg-yellow-400', 'col-1', 'cursor-pointer')
-    liTag.onclick = handleGenerationText
-    listRender.appendChild(liTag)
-}
+const handlRenderProducts = (() => {
+    const menZone = document.getElementById('menZone');
+    console.log({menZone})
+})()
