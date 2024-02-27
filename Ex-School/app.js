0617670b2c6b1f559b0b3fbf6ce135f8a64c65c2
hoanghuynh06599 <hoanghuynh.skyf@gmail.com>
2024-02-27 10:01:06 +0700

    add gitignore

--- Ex-School/app.js
+++ Ex-School/app.js
@@ -1,35 +1,11 @@
-const menProducts = [
-    {
-        id: 1,
-        img: './assets/imgs/product-1.jpg',
-        name: 'Sản phẩm 1',
-        oldPrice: '370.000 VNĐ',
-        newPrice: '269.000 VNĐ'
-    },
-    {
-        id: 2,
-        img: './assets/imgs/product-2.jpg',
-        name: 'Sản phẩm 2',
-        oldPrice: '369.000 VNĐ',
-        newPrice: '249.000 VNĐ'
-    },
-    {
-        id: 3,
-        img: './assets/imgs/product-3.jpg',
-        name: 'Sản phẩm 3',
-        oldPrice: '390.000 VNĐ',
-        newPrice: '159.000 VNĐ'
-    },
-    {
-        id: 4,
-        img: './assets/imgs/product-4.jpg',
-        name: 'Sản phẩm 4',
-        oldPrice: '480.000 VNĐ',
-        newPrice: '209.000 VNĐ'
-    },
-]
+var courses = ["HTML", "CSS", "JavaScript", "PHP"]
+let isFound = false
 
-const handlRenderProducts = (() => {
-    const menZone = document.getElementById('menZone');
-    console.log({menZone})
-})()
+for (let i = 0; i < courses.length; i++) {
+    if(courses[i] === 'PHP') {
+        isFound = true
+        break
+    }
+}
+
+alert(isFound)
