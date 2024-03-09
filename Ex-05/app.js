29ffc65b263428018feb85e730c490d7a4c36c8a
hoanghuynh06599 <hoanghuynh.skyf@gmail.com>
2024-03-09 20:42:51 +0700

    Revert "Update code ex-05"
    This reverts commit 1f08b2c86e82e0e66aa0df3949fd83290d95dd27.

--- Ex-05/app.js
+++ Ex-05/app.js
@@ -4,7 +4,6 @@ const womenItems = [
     {
         id: 1,
         name: 'QUẦN JEAN ỐNG RỘNG',
-        code: "VN01",
         image: "https://down-vn.img.susercontent.com/file/5990e680119e2cb0933685d6f54e6d69",
         oldPrice: "190.000 vnđ",
         newPrice: "125.000 vnđ",
@@ -12,7 +11,6 @@ const womenItems = [
     {
         id: 2,
         name: 'Áo thun nữ croptop tay cộc',
-        code: "VN02",
         image: "https://down-vn.img.susercontent.com/file/d99e4655f596afbfe59873986cf12ceb",
         oldPrice: "58.000 vnđ",
         newPrice: "46.400 vnđ",
@@ -20,7 +18,6 @@ const womenItems = [
     {
         id: 3,
         name: 'Quần thun gân ống rộng',
-        code: "VN03",
         image: "https://down-vn.img.susercontent.com/file/35396454870f1a9d08833d2749b24760",
         oldPrice: "70.000 vnđ",
         newPrice: "49.000 vnđ",
@@ -28,7 +25,6 @@ const womenItems = [
     {
         id: 4,
         name: 'Tất cổ cao nam nữ',
-        code: "VN04",
         image: "https://down-vn.img.susercontent.com/file/6d22ad751dfa4700a0934ed452f0834c",
         oldPrice: "14.000 vnđ",
         newPrice: "6.000 vnđ",
@@ -36,7 +32,6 @@ const womenItems = [
     {
         id: 5,
         name: 'Quần Thun tăm ống rộng',
-        code: "VN05",
         image: "https://down-vn.img.susercontent.com/file/870437a63efdd083b0ecbe0349401217",
         oldPrice: "76.000 vnđ",
         newPrice: "29.000 vnđ",
@@ -44,7 +39,6 @@ const womenItems = [
     {
         id: 6,
         name: 'Balo nữ đi học đi chơi',
-        code: "VN06",
         image: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lrpp3w9wy7x5cb",
         oldPrice: "186.000 vnđ",
         newPrice: "93.000 vnđ",
@@ -55,7 +49,6 @@ const menItems = [
     {
         id: 1,
         name: 'Quần jean nam baggy',
-        code: 'MVN01',
         image: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lozgjkstcevid6",
         oldPrice: "318.000 vnđ",
         newPrice: "175.000 vnđ",
@@ -63,7 +56,6 @@ const menItems = [
     {
         id: 2,
         name: 'Quần Nam Trơn Bo Gấu',
-        code: 'MVN02',
         image: "https://down-vn.img.susercontent.com/file/b6adfd12eb3e66046fc500483c0f3092",
         oldPrice: "106.000 vnđ",
         newPrice: "93.400 vnđ",
@@ -71,7 +63,6 @@ const menItems = [
     {
         id: 3,
         name: 'Áo Thun Polo Nam BL Fashion',
-        code: 'MVN03',
         image: "https://down-vn.img.susercontent.com/file/sg-11134201-23010-zvs6p52ndwmve6",
         oldPrice: "150.000 vnđ",
         newPrice: "28.000 vnđ",
@@ -79,7 +70,6 @@ const menItems = [
     {
         id: 4,
         name: 'Áo Polo Nam Cổ Bẻ Thun',
-        code: 'MVN04',
         image: "https://down-vn.img.susercontent.com/file/743050d3c10a59f1b911810ead366116",
         oldPrice: "69.000 vnđ",
         newPrice: "29.000 vnđ",
@@ -87,7 +77,6 @@ const menItems = [
     {
         id: 5,
         name: 'Thắt lưng nam da cao cấp ',
-        code: 'MVN05',
         image: "https://down-vn.img.susercontent.com/file/vn-11134207-7r98o-lpfn82x8ovv2a2",
         oldPrice: "19.000 vnđ",
         newPrice: "5.000 vnđ",
@@ -95,7 +84,6 @@ const menItems = [
     {
         id: 6,
         name: 'Áo sơ mi nam kiểu dáng basic',
-        code: 'MVN06',
         image: "https://down-vn.img.susercontent.com/file/sg-11134201-23010-uc3sy3gvivlvbb",
         oldPrice: "199.000 vnđ",
         newPrice: "99.000 vnđ",
@@ -132,56 +120,50 @@ const newItems = [
 const handleRenderProducts = () => {
     womenItems.forEach((item) => {
         const womenZone = $('#women')
-        const trTag = document.createElement('tr')
-        trTag.classList.add('odd:bg-white', 'even:bg-gray-50', 'border-b')
-        trTag.innerHTML = `
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                ${item.id}
-            </th>
-            <td class="px-6 py-4">
-                ${item.code}
-            </td>
-            <td class="px-6 py-4">
-                ${item.name}
-            </td>
-            <td class="px-6 py-4">
-                <img class="rounded-lg w-16 h-16" src="${item.image}" alt="" />
-            </td>
-            <td class="px-6 py-4">
-                ${item.oldPrice}
-            </td>
-            <td class="px-6 py-4">
-                ${item.newPrice}
-            </td>
+        const liTag = document.createElement('li')
+        liTag.classList.add('col-1', 'max-w-sm', 'bg-white', 'border', 'border-gray-200', 'rounded-lg', 'shadow')
+        liTag.innerHTML = `
+            <img class="rounded-t-lg" src="${item.image}" alt="" />
+            <div class="p-5">
+                <a href="#">
+                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 ">${item.name}</h5>
+                </a>
+                <p class="mb-5 font-normal text-gray-700 flex items-center justify-start gap-12">
+                    <span>${item.newPrice}</span>
+                    <span class="line-through">${item.oldPrice}</span>
+                </p>
+                <button
+                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-100"
+                >
+                    Mua ngay
+                </button>
+            </div>
         `
-        womenZone.appendChild(trTag)
+        womenZone.appendChild(liTag)
     })
 
     menItems.forEach((item) => {
         const menZone = $('#men')
-        const trTag = document.createElement('tr')
-        trTag.classList.add('odd:bg-white', 'even:bg-gray-50', 'border-b')
-        trTag.innerHTML = `
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                ${item.id}
-            </th>
-            <td class="px-6 py-4">
-                ${item.code}
-            </td>
-            <td class="px-6 py-4">
-                ${item.name}
-            </td>
-            <td class="px-6 py-4">
-                <img class="rounded-lg w-16 h-16" src="${item.image}" alt="" />
-            </td>
-            <td class="px-6 py-4">
-                ${item.oldPrice}
-            </td>
-            <td class="px-6 py-4">
-                ${item.newPrice}
-            </td>
+        const liTag = document.createElement('li')
+        liTag.classList.add('col-1', 'max-w-sm', 'bg-white', 'border', 'border-gray-200', 'rounded-lg', 'shadow')
+        liTag.innerHTML = `
+            <img class="rounded-t-lg" src="${item.image}" alt="" />
+            <div class="p-5">
+                <a href="#">
+                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900 ">${item.name}</h5>
+                </a>
+                <p class="mb-5 font-normal text-gray-700 flex items-center justify-start gap-12">
+                    <span>${item.newPrice}</span>
+                    <span class="line-through">${item.oldPrice}</span>
+                </p>
+                <button
+                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-100"
+                >
+                    Mua ngay
+                </button>
+            </div>
         `
-        menZone.appendChild(trTag)
+        menZone.appendChild(liTag)
     })
 
     newItems.forEach((item) => {
